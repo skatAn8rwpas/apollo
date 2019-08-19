0a77d9e2aa652ec2849ac76fbadafff2fbef0d8f
SkatAn8rwpos <skatan8rwpos@gmail.com>
2019-08-19 15:56:07 +0300

    Add mongoDB and Atlas

--- index.js
+++ index.js
@@ -1,51 +1,70 @@
 const { ApolloServer, gql } = require('apollo-server-micro');
+const { connect } = require('./src/config')
+const {ObjectID} = require('mongodb')
 
-const books = [
-  { id: 1, title: 'The Trials of Brother Jero', rating: 8, authorId: 1 },
-  { id: 2, title: 'Half of a Yellow Sun', rating: 9, authorId: 3 },
-  { id: 3, title: 'Americanah', rating: 9, authorId: 3 },
-  { id: 4, title: 'King Baabu', rating: 9, authorId: 1 },
-  { id: 5, title: 'Children of Blood and Bone', rating: 8, authorId: 2 },
-];
 
-const authors = [
-  { id: 1, firstName: 'Wole', lastName: 'Soyinka' },
-  { id: 2, firstName: 'Tomi', lastName: 'Adeyemi' },
-  { id: 3, firstName: 'Chimamanda', lastName: 'Adichie' },
-];
+
+
+// let bookId = 5;
+// const books = [
+//   { id: 1, title: 'The Trials of Brother Jero', rating: 8, authorId: 1 },
+//   { id: 2, title: 'Half of a Yellow Sun', rating: 9, authorId: 3 },
+//   { id: 3, title: 'Americanah', rating: 9, authorId: 3 },
+//   { id: 4, title: 'King Baabu', rating: 9, authorId: 1 },
+//   { id: 5, title: 'Children of Blood and Bone', rating: 8, authorId: 2 },
+// ];
+
+// const authors = [
+//   { id: 1, firstName: 'Wole', lastName: 'Soyinka' },
+//   { id: 2, firstName: 'Tomi', lastName: 'Adeyemi' },
+//   { id: 3, firstName: 'Chimamanda', lastName: 'Adichie' },
+// ];
 
 const typeDefs = gql`
   type Author {
-    id: Int!
+    _id: String!
     firstName: String!
     lastName: String!
     books: [Book]! # the list of books by this author
   }
+
   type Book {
-    id: Int!
+    _id: String!
     title: String!
     rating: Int!
     author: Author!
   }
+
   # the schema allows the following query
   type Query {
     books: [Book!]!
-    book(id: Int!): Book!
-    author(id: Int!): Author!
+    book(_id: String!): Book!
+    authors: [Author!]!
+    author(_id: String!): Author!
   }
+
   # this schema allows the following mutation
   type Mutation {
-    addBook(title: String!, rating: Int!, authorId: Int!): Book!
+    addBook(title: String!, rating: Int!, authorId: String!): Book!
+    addAuthor(firstName: String!, lastName: String!): Author!
   }
 `;
 
-let bookId = 5;
 
 const resolvers = {
   Query: {
-    books: () => books,
-    book: (_, { id }) => books.find(book => book.id === id),
-    author: (_, { id }) => authors.find(author => author.id === id),
+    books: () => connect().then(async (client) =>
+      await client.db('test').collection('books').find().toArray()
+    ),
+    book: (root, { _id }) => connect().then(async (client) =>
+      await client.db('test').collection('books').findOne({_id: new ObjectID(_id)})
+    ),
+    authors: () => connect().then(async (client) =>
+      await client.db('test').collection('authors').find().toArray()
+    ),
+    author: (_, { _id }) => connect().then(async (client) =>
+      await client.db('test').collection('authors').findOne({_id: new ObjectID(_id)})
+    ),
   },
   Mutation: {
     addBook: (_, { title, rating, authorId }) => {
@@ -61,12 +80,23 @@ const resolvers = {
       books.push(newBook);
       return newBook;
     },
+
+    addAuthor: (root, {firstName, lastName}) => connect().then(async client => {
+      const result = await client.db('test').collection('authors').insertOne({ firstName, lastName })
+      console.log(result)
+      return result.ops[0]
+    })
   },
   Author: {
-    books: author => books.filter(book => book.authorId === author.id),
+    books: author => connect().then(async (client) =>
+      await client.db('test').collection('books').find({authorId: new ObjectID(author._id)}).toArray()
+    )
   },
+
   Book: {
-    author: book => authors.find(author => author.id === book.authorId),
+    author: book => connect().then(async (client) =>
+      await client.db('test').collection('authors').findOne({_id: new ObjectID(book.authorId)})
+    )
   },
 };
 
